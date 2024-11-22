ac03642013c909e5e70a4446af8f28751d7231dc
Gabriel Vara <gabriel.alvesvara@outlook.com>
2024-11-22 16:16:23 -0300

    Corrigindo erro

--- src/pages/GreenflowPages/GreenflowAccount.jsx
+++ src/pages/GreenflowPages/GreenflowAccount.jsx
@@ -109,7 +109,7 @@ function GreenflowAccount() {
       {/* Botão Sair */}
       <button
         onClick={handleLogout}
-        className="mt-8 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+        className="mt-8 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-2xl"
       >
         Sair
       </button>
